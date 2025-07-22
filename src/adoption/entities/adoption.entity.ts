b73c3fc4f3aadcb62f5b6c986412da8a3ca9986f
luis.talero <luis.talero@wiedii.co>
2025-07-22 13:25:21 -0500

    feat: :sparkle: se realizan cambios en la entidad adoption

--- src/adoption/entities/adoption.entity.ts
+++ src/adoption/entities/adoption.entity.ts
@@ -1,6 +1,6 @@
 import { Cat } from "src/cats/entities/cat.entity";
 import { Owner } from "src/owner/entities/owner.entity";
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, DeleteDateColumn } from "typeorm";
 
 @Entity()
 export class Adoption {
@@ -21,7 +21,8 @@ export class Adoption {
     @Column()
     adoptionDate: Date;
 
-    @Column({ type: 'timestamp', nullable: true })
+    @DeleteDateColumn()
     deletedAt: Date;
 
 }
+
